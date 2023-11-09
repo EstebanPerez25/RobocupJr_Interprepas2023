e7387157a0209316bf81b51afc4053b8cd33f4fd
joeManuel19 <116661936+joeManuel19@users.noreply.github.com>
2023-11-08 21:11:48 -0600

    Update Dispenser.h

--- main_2.0_nuevo_clases_______/Dispenser.h
+++ main_2.0_nuevo_clases_______/Dispenser.h
@@ -3,36 +3,60 @@
 
 class Dispenser {
 
-private:
-  uint8_t sm_Dis;
+  private:
+    uint8_t sm_Dis;
 
-  // OBJECTS
-   Servo servoDis;  // front-left
+    // OBJECTS
+    Servo servoDis;  // front-left
 
-public:
-Dispenser(uint8_t sm_Dis);
+  public:
+    Dispenser(uint8_t sm_Dis);
 
-byte action;
+    byte action;
 
-// METHODS
-byte Disp(byte action);
+    // METHODS
+    byte drop(byte action);
 
 };
-Dispenser::Dispenser(){
+
+
+Dispenser::Dispenser(uint8_t _sm_Dis) {
+  sm_Dis = _sm_Dis;
+
+
+  servoDis.attach(sm_Dis);
+  servoDis.write(90);
+  servoDis.detach();
+
+
   Serial.begin(9600);
   Serial.println("OK COLORSENSOR");
 }
-byte Dispenser::Disp(byte action){
 
-switch(action){
-case 0:
 
-break;
-case 1:
+byte Dispenser::drop(byte action) {
+  servoDis.attach(sm_Dis);
 
-break;
-  
-}
+  switch (action) {
+    case 0:
+      servoDis.write(120);
+      delay(500);
+      servoDis.write(90);
+      delay(500);
+      break;
 
-  
+
+    case 1:
+      servoDis.write(120);
+      delay(500);
+      servoDis.write(90);
+      delay(500);
+      servoDis.write(120);
+      delay(500);
+      servoDis.write(90);
+      delay(500);
+      break;
+  }
+
+  servoDis.detach();
 }
