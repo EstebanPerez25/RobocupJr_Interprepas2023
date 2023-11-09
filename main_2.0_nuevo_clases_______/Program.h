64676b866cf83561c3101dfdc734fb084c3d2268
joeManuel19 <116661936+joeManuel19@users.noreply.github.com>
2023-11-08 21:11:56 -0600

    Update Program.h

--- main_2.0_nuevo_clases_______/Program.h
+++ main_2.0_nuevo_clases_______/Program.h
@@ -2,7 +2,9 @@
 #include "Movements.h"
 #include "Ultrasonics.h"
 #include "ColorSens.h"
-//#include "Oled.h"
+#include "Oled.h"
+#include "Dispenser.h"
+#include "CameraESP.h"
 
 
 // PINOUT
@@ -11,7 +13,10 @@
 #define sm_BL 3
 #define sm_FR 5
 #define sm_BR 6
-
+#define sm_Dis 9
+#define p1 10
+#define p2 11
+#define p3 12
 
 // mesures of the ultrasonics .
 #define dwall 11
@@ -21,7 +26,9 @@
 Movements Maze(sm_FL, sm_BL, sm_FR, sm_BR);    // objeto de clase Movements.h
 Ultrasonics Sensors;                           // objeto de clase Ultrasonics.h
 ColorSens floorColor;                          // objeto de clase ColorSens.h
-//Oled oled;                                   // objeto de clase Oled.h
+Oled oled;                                    // objeto de clase Oled.h
+Dispenser disp(sm_Dis);                       // objeto de clase Dispenser.h
+CameraESP Cam(p1, p2, p3);                       // objeto de clase Dispenser.h
 
 
 
@@ -31,7 +38,7 @@ class Program {    // se crea clase Program
     // Atributes
     byte action_us;   // Move suggested by ultrasonic sensors
     byte action_cs;   // Move suggested by color sensor
-    //    byte action_cam;  // Move suggested by the camera
+    byte action_cam;  // Move suggested by the camera
     byte action;      // The next action
 
   public:
@@ -39,7 +46,7 @@ class Program {    // se crea clase Program
 
 
     // Methods
-    byte navigation(byte action_us, byte action_cs/*, byte action_cam*/);  // Takes the actions ssuggested by the sensors and takes a decision
+    byte navigation(byte action_us, byte action_cs, byte action_cam);  // Takes the actions ssuggested by the sensors and takes a decision
     void algoritm();
 
 };
@@ -51,16 +58,18 @@ Program::Program() {
 }
 
 
-byte Program::navigation(byte action_us, byte action_cs/*, byte action_cam*/) {
+byte Program::navigation(byte action_us, byte action_cs, byte action_cam) {
 
   switch (action_cs) {
     case 5:
       // Black floor
+      oled.showDisp(5);
       return 5;
       break;
 
     case 6:
       // Blue
+      oled.showDisp(6);
       return 6;
       break;
 
@@ -71,6 +80,10 @@ byte Program::navigation(byte action_us, byte action_cs/*, byte action_cam*/) {
 
     case 8:
       // White
+      if (action_cam != 4) {
+        Maze.moves(7);  // se para el robot
+        disp.drop(action_cam);
+      }
       return action_us;
   }
 }
@@ -83,12 +96,12 @@ void Program::algoritm() {
   action_us = Sensors.action();
   action_cs = floorColor.action();
 
-  //  action_cam = 0;//Cam.action();
+  action_cam = Cam.action();
 
   // 2. Decide the next movement
   action = navigation(action_us, action_cs/*, action_cam*/);
   Serial.println(action);
-  
+
   //3. Movement
   Maze.moves(action);
 
