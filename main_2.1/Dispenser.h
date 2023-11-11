44c314ba5b74d4f63cbde8966167572fa9c3f4b4
joeManuel19 <116661936+joeManuel19@users.noreply.github.com>
2023-11-11 09:07:21 -0600

    Create Dispenser.h

---
+++ main_2.1/Dispenser.h
@@ -0,0 +1,62 @@
+// LIBRARIES
+#include <Servo.h>
+
+class Dispenser {
+
+  private:
+    uint8_t sm_Dis;
+
+    // OBJECTS
+    Servo servoDis;  // front-left
+
+  public:
+    Dispenser(uint8_t sm_Dis);
+
+    byte action;
+
+    // METHODS
+    byte drop(byte action);
+
+};
+
+
+Dispenser::Dispenser(uint8_t _sm_Dis) {
+  sm_Dis = _sm_Dis;
+
+
+  servoDis.attach(sm_Dis);
+  servoDis.write(90);
+  servoDis.detach();
+
+
+  Serial.begin(9600);
+  Serial.println("OK DISPENSER");
+}
+
+
+byte Dispenser::drop(byte action) {
+  servoDis.attach(sm_Dis);
+
+  switch (action) {
+    case 0:
+      servoDis.write(120);
+      delay(500);
+      servoDis.write(170);
+      delay(500);
+      break;
+
+
+    case 1:
+      servoDis.write(120);
+      delay(500);
+      servoDis.write(170);
+      delay(500);
+      servoDis.write(120);
+      delay(500);
+      servoDis.write(170);
+      delay(500);
+      break;
+  }
+
+  servoDis.detach();
+}
